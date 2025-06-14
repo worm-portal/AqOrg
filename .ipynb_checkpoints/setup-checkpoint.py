51d719db295b4aa027a804bfa9cc4dd05d9ca0cc
gmboyer <gmboyer@asu.edu>
2025-06-14 10:56:20 -0700

    latest commit before testing on docker

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AqOrg",
-    version="0.2.1",
+    version="0.3.0",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Estimate thermodynamic properties of aqueous organic compounds",
