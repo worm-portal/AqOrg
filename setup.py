6e261325ddf23ccfce547d91aa2233166a441df2
Grayson Boyer <gmboyer@asu.edu>
2025-06-18 22:43:19 +0000

    commit before packaging for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AqOrg",
-    version="0.3.0",
+    version="0.4.0",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Estimate thermodynamic properties of aqueous organic compounds",
