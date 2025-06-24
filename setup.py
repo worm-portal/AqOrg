0ed17eeb15b6d318bb2b6e0efff019b4f323b435
gmboyer <gmboyer@asu.edu>
2025-06-24 11:40:48 -0700

    updated setup.py and citation.cff

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AqOrg",
-    version="0.4.0",
+    version="0.4.1",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Estimate thermodynamic properties of aqueous organic compounds",
